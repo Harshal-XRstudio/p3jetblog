e2c2bab727537f0d46375ef8894c62ceceffb211
Harshal-XRstudio <harshal.xrstudio@gmail.com>
2025-11-10 11:21:25 +0530

    add adsense script

--- src/app/layout.js
+++ src/app/layout.js
@@ -22,6 +22,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2234384779164146"
+          crossorigin="anonymous"></script>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col min-h-screen bg-white dark:bg-gray-900 text-gray-900 dark:text-gray-100`}
       >
